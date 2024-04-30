83731322732c9c655f31113362911a4137922b17
Le Van Phuc <levanphuc@MacBook-Air-cua-Le-4.local>
2024-05-01 00:07:53 +0700

    Landing page - Project 2

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@
  * 
 */
 
+const sections = document.querySelectorAll('section');
+const navbar = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
@@ -39,24 +41,45 @@
 */
 
 // build the nav
+function buildNav() {
+  sections.forEach(function(section) {
+    const navItem = document.createElement('li');
+    navItem.innerHTML = `<a class="nav__link menu__link" href="#${section.id}" id="navli"> ${section.dataset.nav}</a>`;
+    navbar.appendChild(navItem);  
+  });
+
+}
 
 
 // Add class 'active' to section when near top of viewport
+function setActiveSection() {
+  const navbarLinks = document.querySelectorAll('#navli');
+  sections.forEach(function(section) {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 150 && rect.bottom >=150) {
+      section.classList.add('active');
+      navbarLinks.forEach(function(link) {
+        if (link.getAttribute('href').slice(1) === section.id) {
+          link.classList.add('active__link');
+        } else {
+          link.classList.remove('active__link');
+        }
+      });
+    } else {
+      section.classList.remove('active');
+    }
+  });
+}
 
 
-// Scroll to anchor ID using scrollTO event
+// Add event listener to listen for scroll events
+window.addEventListener('scroll', setActiveSection);
 
+// Call buildNav to generate navigation menu
+buildNav();
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
